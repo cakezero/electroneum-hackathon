96d56f9ceb3f71738e2374cf7294f489de0d9673
cakezero <sareahj.com@gmail.com>
2025-01-26 02:32:59 +0100

    frontend init, smart contract finalisation(NFT contract, deployment and tests)

---
+++ smart-contract/contracts/CertNFT.sol
@@ -0,0 +1,34 @@
+// SPDX-LICENSE-IDENTIFIER: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+
+contract CertNFT is ERC721 {
+  string public baseURI;
+  uint public currentTokenId;
+  address public owner;
+
+  constructor (string memory name, string memory symbol) ERC721(name, symbol) {
+    owner = msg.sender;
+  }
+
+  function mint() external {
+    currentTokenId += 1;
+    uint newTokenId = currentTokenId;
+    _safeMint(msg.sender, newTokenId);
+  }
+
+  function setBaseURI(string memory newBaseURI) external onlyOwner {
+    baseURI = newBaseURI;
+  }
+
+  function _baseURI() internal override view returns (string memory) {
+    return baseURI;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "You can't perform this operation");
+    _;
+  }
+}
--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -1,8 +1,17 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { vars, type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+const ANKR_RPC = vars.get("ANKR_RPC")
+const PRIVATE_KEY = vars.get("PRIVATE_KEY")
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    electroneum: {
+      url: "https://rpc.ankr.com/electroneum_testnet/fa11daf359979d274ae40321eabc6ae4691980bd7576b0ff21c4cba6ce3e4912",
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
